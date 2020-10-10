3a6b01a64ad73a45ab7878f4c6adb9ace7e7f002
liyang <leonvincent94@outlook.com>
2020-10-10 08:39:58 +0800

    数据加密

--- src/controller/user.js
+++ src/controller/user.js
@@ -1,11 +1,14 @@
 const { exec, escape } = require('../db/mysql')
+const { genPassword } = require('./../utils/cryp')
+
 const login = (username, password) => {
   username = escape(username)
+  password = genPassword(password)
   password = escape(password)
+  
   const sql = `
     select username, realname from users where username=${username} and password=${password};
   `
-  console.log('sql is', sql)
   return exec(sql).then(rows => {
     console.log('rows---------------->', rows)
     return rows[0] || {}
